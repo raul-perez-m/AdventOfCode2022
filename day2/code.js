eb1d36f40e89b092f6f0266a8786586188f76852
Raúl Pérez <raul272@gmail.com>
2022-12-02 10:15:52 +0100

    Fix

--- day2/code.js
+++ day2/code.js
@@ -10,7 +10,9 @@ function calculate(){
         values[0] = values[0] == 'A' ? 1 : values[0] == 'B' ? 2 : 3; 
         values[1] = values[1] == 'X' ? 1 : values[1] == 'Y' ? 2 : 3; 
         let result = values[0] == values[1] ? 3 : 0;
-        if( values[0] == 3 && values[1] == 1 || values[0] == 2 && values[1] == 3 || values[0] == 1 && values[1] == 2){
+        if(values[0] == values[1]) {
+            result = 3;
+        } else if( values[0] == 3 && values[1] == 1 || values[0] == 2 && values[1] == 3 || values[0] == 1 && values[1] == 2){
             result = 6;
         }
     
@@ -20,16 +22,17 @@ function calculate(){
         })
     });
 
-    const total = array.reduce((p, c) => p += c.figure + c.state , 0 );
+    const total =array.reduce((p, c) => p += c.figure + c.state , 0 );
     console.log(total);
-
     const array2 = []
     input.split('\n').forEach((x) => {
         const values = x.split(' ');
         values[0] = values[0] == 'A' ? 1 : values[0] == 'B' ? 2 : 3; 
         let result = values[1] == 'X' ? 0 : values[1] == 'Y' ? 3 : 6;
-        let figure = result == 3 ? values[0] : 0;
-        if (result == 0) {
+        let figure = 0;
+        if(result == 3) {
+            figure = values[0];
+        } else if (result == 0) {
             figure = values[0] == 3 ? 2 : values[0] == 2 ? 1 : 3
         } else {
             figure = values[0] == 3 ? 1 : values[0] == 2 ? 3 : 2
