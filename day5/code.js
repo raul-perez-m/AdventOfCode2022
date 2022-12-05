4e5c7d307af55a8bb93bbaab988ce5fadb5f34d2
Raúl Pérez <raul272@gmail.com>
2022-12-05 10:20:09 +0100

    day5

---
+++ day5/code.js
@@ -0,0 +1,68 @@
+const { readFileSync } = require('fs');
+const  path = require('path')
+
+const input = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+
+const crates = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: []
+}
+
+function moveCrate(k, from, to){
+    for(let i = 0; i < k; i +=1)
+    {
+        const moving = crates[from].shift()
+        crates[to].unshift(moving);
+    }
+}
+
+function moveCrateMultiple(k, from, to){
+    const moving = crates[from].splice(0, k);
+    crates[to].unshift(...moving);
+    
+}
+
+function calculate(){
+    const data = input.split('\n');
+    const plane = data.splice(0, 9);
+    console.log(plane);
+    plane.pop();
+    const lines = []
+    plane.forEach((line) => {
+        let spaces = 0;
+        let fixedLine = [];
+        line.split(' ').forEach(x => {
+            if(!x) {
+                spaces += 1;
+                if(spaces === 4){
+                    fixedLine.push('[]');
+                    spaces = 0;
+                }
+            }
+            else fixedLine.push(x);
+        })
+        lines.push(fixedLine);
+    });
+
+    lines.forEach((l) => {
+        l.forEach((x,idx) => x !== '[]' ? crates[idx + 1].push(x) : '')
+    })
+    data.filter(x => x).forEach(line => {
+        const move = line.split(' ').filter(Number).map(Number);
+        moveCrateMultiple(move[0], move[1], move[2])
+        //moveCrate(move[0], move[1], move[2])
+    });
+
+    const output = Object.values(crates).reduce((p,c) => p += c[0], '')
+    console.log(output)
+ 
+}
+
+calculate();
