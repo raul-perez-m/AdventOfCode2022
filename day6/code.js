8002030db0e11b9cffc2ed8ea90c183eef2e82e3
Raúl Pérez <raul272@gmail.com>
2022-12-06 09:48:39 +0100

    day6

---
+++ day6/code.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require('fs');
+const  path = require('path')
+
+const input = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+
+function checkString(str) {
+    return new Set(str).size == str.length;
+}
+
+function calculateIndex(input, lenght){
+    for(let i = 0; i < input.length; i += 1){
+        const slice = input.substring(i, i + lenght,)
+        if (checkString(slice)){
+            return i;
+        }
+    }   
+}
+
+function calculate(){
+    console.log(calculateIndex(input, 4) + 4)
+    console.log(calculateIndex(input, 14) + 14)
+}
+
+calculate();
